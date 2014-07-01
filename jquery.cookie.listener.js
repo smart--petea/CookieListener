50b9bdf01dd24dbb3c6773008602b0c434094938
petea <smart__petea@mail.ru>
2014-07-01 21:57:52 +0200

    petea - defined offchange function

--- jquery.cookie.listener.js
+++ jquery.cookie.listener.js
@@ -1,5 +1,4 @@
 (function($) {
-	//todo: rewrite removeCookie
 	if ( $ !== jQuery ) {
 		throw new Error("jQuery is not connected"); 
 	}
@@ -10,8 +9,8 @@
 
 	/* var */ $cookie = $.cookie, //store original cookie
 		refresh_interval = 1000, //to refresh every 100 ms
-		callback_store = {},
-		cookie_vals = {};
+		change_callbacks = {},
+		change_vals = {};
 	
 	CookieStorage = $.cookie = function() {
 		var args = Array.prototype.slice.call(arguments);
@@ -32,8 +31,7 @@
 
 
 	CookieStorage.onchange = function (cookie_name, callback, context) {
-		//todo: verify params
-		var cookie_callbacks = callback_store[cookie_name] = callback_store[cookie_name] || [];
+		var cookie_callbacks = change_callbacks[cookie_name] = change_callbacks[cookie_name] || [];
 
 		cookie_callbacks.push({
 			callback: callback,
@@ -41,15 +39,15 @@
 		});
 
 		//store cookie val
-		cookie_vals[cookie_name] = $cookie.call($, cookie_name);
+		change_vals[cookie_name] = $cookie.call($, cookie_name);
 	}
 
 
 	/* intern functions */
 	function check_cookie() {
 		var cookie_name, old_val, new_val;
-		for(cookie_name in cookie_vals) {
-			old_val = cookie_vals[cookie_name];
+		for(cookie_name in change_vals) {
+			old_val = change_vals[cookie_name];
 			new_val = $cookie.call($, cookie_name);
 			old_val != new_val && change_trigger(cookie_name, old_val, new_val);
 		};
@@ -58,11 +56,11 @@
 	setInterval(check_cookie, refresh_interval);
 
 	function change_trigger(cookie_name, old_val, new_val) {
-		if( ! (callback_store[cookie_name] instanceof Array) ) {
+		if( ! (change_callbacks[cookie_name] instanceof Array) ) {
 			return;
 		};  
 
-		cookie_vals[cookie_name] = new_val;
+		change_vals[cookie_name] = new_val;
 
 		var eve = {
 			name: cookie_name,
@@ -71,13 +69,35 @@
 		};
 
 		
-		$(callback_store[cookie_name]).each(function(_, record) {
+		$(change_callbacks[cookie_name]).each(function(_, record) {
 			record.callback.call(record.context, eve);
 		});
 	}
 
 	CookieStorage.offchange = function(cookie_name, callback, context) {
-		throw new Error('not implemented');	
+		var callbacks = change_callbacks[cookie_name];
+		if( ! callbacks ) {
+			return ;
+		}
+
+		if( ! callback ) {
+			//remove all listeners for this cookie
+			callbacks.splice(0);
+			return;
+		}
+
+		context = context || null;
+
+		if( ! $.isFunction(callback)) {
+			throw new Error("callback must be a function");
+		}
+
+		var i;
+		for( i in change_callbacks[cookie_name] ) {
+			if(callbacks[i].callback === callback && callbacks[i].context === context) {
+				callbacks.splice(i, 1);
+			}
+		}
 	}
 
 })(jQuery)
